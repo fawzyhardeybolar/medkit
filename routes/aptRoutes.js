6193f701ce5cc4a1b85871064a8a53105b3f9054
fawzyhardeybolar <fawazhardeybolar2002@gmail.com>
2023-04-24 13:04:35 +0100

    remove code

--- routes/aptRoutes.js
+++ routes/aptRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const { auth } = require("../middleware/auth");
-const { createAppointment, getAppointments } = require("../controllers/aptControllers");
+const {
+  createAppointment,
+  getAppointments,
+} = require("../controllers/aptControllers");
 
-router.route("/").post(auth, createAppointment).get(auth, getAppointments);
+router.route("/").post(auth, createAppointment).get(getAppointments);
 
 module.exports = router;
