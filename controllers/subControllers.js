ffb1931c882bf69a3210209a80c2d9037ff65706
fawzyhardeybolar <fawazhardeybolar2002@gmail.com>
2023-04-27 10:39:54 +0100

    add code

--- controllers/subControllers.js
+++ controllers/subControllers.js
@@ -1,4 +1,4 @@
-const Subscribe = require("../models/Subscribe")
+const Subscribe = require("../models/Subscribe");
 
 const subscribe = async (req, res) => {
   try {
@@ -7,6 +7,14 @@ const subscribe = async (req, res) => {
       return res
         .status(400)
         .json({ success: false, msg: "Pls provide your email to subscribe" });
+    }
+
+    const emailExist = await Subscribe.findOne({ email });
+
+    if (emailExist) {
+      return res
+        .status(400)
+        .json({ msg: "This email has already been added to our newsletter" });
     }
 
     const sub = await Subscribe.create(req.body);
